00f27fb871502d85cab800778f883943fd6d942d
Denis Dublennykh <stigius@kontur.ru>
2014-06-03 17:53:39 +0600

    Минимальная заготовка генератора excel'евских файлов

---
+++ ExcelDocumentFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace SKBKontur.Catalogue.ExcelFileGenerator
+{
+    public static class ExcelDocumentFactory
+    {
+        public static IExcelDocument CreateFromTemplate(byte[] template)
+        {
+            return new ExcelDocument(template);
+        }
+    }
+}
---
+++ IExcelDocument.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+using DocumentFormat.OpenXml.Packaging;
+
+namespace SKBKontur.Catalogue.ExcelFileGenerator
+{
+    public interface IExcelDocument
+    {
+        byte[] GetDocumentBytes();
+    }
+
+    internal class ExcelDocument : IExcelDocument, IDisposable
+    {
+        public ExcelDocument(byte[] template)
+        {
+            documentMemoryStream = new MemoryStream();
+            documentMemoryStream.Write(template, 0, template.Length);
+            spreadsheetDocument = SpreadsheetDocument.Open(documentMemoryStream, true);
+        }
+
+        public void Dispose()
+        {
+            spreadsheetDocument.Dispose();
+            documentMemoryStream.Dispose();
+        }
+
+        public byte[] GetDocumentBytes()
+        {
+            return documentMemoryStream.ToArray();
+        }
+
+        private readonly MemoryStream documentMemoryStream;
+        private readonly SpreadsheetDocument spreadsheetDocument;
+    }
+}
