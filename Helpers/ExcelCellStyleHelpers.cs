eea880648b48f90b12004207d36f0a973591a2a1
Denis Dublennykh <stigius@kontur.ru>
2014-06-13 18:40:12 +0600

    Fluent interface для изменения стилей

---
+++ Helpers/ExcelCellStyleHelpers.cs
@@ -0,0 +1,64 @@
+﻿using GroBuf;
+using GroBuf.DataMembersExtracters;
+
+using SKBKontur.Catalogue.ExcelFileGenerator.DataTypes;
+
+namespace SKBKontur.Catalogue.ExcelFileGenerator.Helpers
+{
+    public static class ExcelCellStyleHelpers
+    {
+        public static ExcelCellStyle FontSize(this ExcelCellStyle style, int fontSize)
+        {
+            var result = serializer.Copy(style);
+            if(result.FontStyle == null)
+                result.FontStyle = new ExcelCellFontStyle();
+            result.FontStyle.Size = fontSize;
+            return result;
+        }
+
+        public static ExcelCellStyle FontColor(this ExcelCellStyle style, ExcelColor color)
+        {
+            var result = serializer.Copy(style);
+            if(result.FontStyle == null)
+                result.FontStyle = new ExcelCellFontStyle();
+            result.FontStyle.Color = color;
+            return result;
+        }
+
+        public static ExcelCellStyle Bold(this ExcelCellStyle style)
+        {
+            var result = serializer.Copy(style);
+            if(result.FontStyle == null)
+                result.FontStyle = new ExcelCellFontStyle();
+            result.FontStyle.Bold = true;
+            return result;
+        }
+
+        public static ExcelCellStyle Underlined(this ExcelCellStyle style)
+        {
+            var result = serializer.Copy(style);
+            if(result.FontStyle == null)
+                result.FontStyle = new ExcelCellFontStyle();
+            result.FontStyle.Underlined = true;
+            return result;
+        }
+
+        public static ExcelCellStyle Borders(this ExcelCellStyle style, ExcelCellBordersStyle borders)
+        {
+            var result = serializer.Copy(style);
+            result.BordersStyle = borders;
+            return result;
+        }
+
+        public static ExcelCellStyle Numeric(this ExcelCellStyle style, int precision)
+        {
+            var result = serializer.Copy(style);
+            if(result.NumberingFormat == null)
+                result.NumberingFormat = new ExcelCellNumberingFormat();
+            result.NumberingFormat.Precision = precision;
+            return result;
+        }
+
+        private static readonly ISerializer serializer = new Serializer(new AllFieldsExtractor());
+    }
+}
