8551757d288c2aff6b9a70de8845dae0678887cc
ageev_ps <ageev_ps@skbkontur.ru>
2015-01-23 16:49:53 +0500

    Core.ExcelFileGenerator: Добавил возможность извлечения из документа ячеек из некоторого диапазона

---
+++ DocumentPartsExtractionTests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
+
+using SKBKontur.Catalogue.ExcelFileGenerator;
+
+namespace SKBKontur.Catalogue.Core.Tests.ExcelFileGeneratorTests
+{
+    [TestFixture]
+    public class DocumentPartsExtractionTests
+    {
+        [Test]
+        public void CellsInRangeTest()
+        {
+            var document = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(fileName));
+            var worksheet = document.GetWorksheet(0);
+            var cells = worksheet.GetSortedCellsInRange("A34", "B38").ToArray();
+
+            Assert.AreEqual(10, cells.Count());
+            Assert.AreEqual("Value:String:Name", cells[1].GetStringValue());
+            Assert.AreEqual("Value:String:Address", cells[9].GetStringValue());
+
+            document.Dispose();
+        }
+
+        private const string fileName = @"ExcelFileGeneratorTests\Files\template.xlsx";
+    }
+}
