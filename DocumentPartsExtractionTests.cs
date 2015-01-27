5b6aeb6a65e6417e2b44933c3aba1fb0c6740ed5
ageev_ps <ageev_ps@skbkontur.ru>
2015-01-27 18:22:56 +0500

    Core.ExcelFileGenerator: Дописал примитивов и переделал все индексы на ExcelCellIndex

--- DocumentPartsExtractionTests.cs
+++ DocumentPartsExtractionTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using NUnit.Framework;
 
 using SKBKontur.Catalogue.ExcelFileGenerator;
+using SKBKontur.Catalogue.ExcelFileGenerator.Implementation;
 
 namespace SKBKontur.Catalogue.Core.Tests.ExcelFileGeneratorTests
 {
@@ -15,11 +16,39 @@ namespace SKBKontur.Catalogue.Core.Tests.ExcelFileGeneratorTests
         {
             var document = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(fileName));
             var worksheet = document.GetWorksheet(0);
-            var cells = worksheet.GetSortedCellsInRange("A34", "B38").ToArray();
 
-            Assert.AreEqual(10, cells.Count());
+            var upperLeft = new ExcelCellIndex("A22");
+            var lowerRight = new ExcelCellIndex("A24");
+            var cells = worksheet.GetSortedCellsInRange(upperLeft, lowerRight).ToArray();
+
+            Assert.AreEqual(3, cells.Count());
             Assert.AreEqual("Value:String:Name", cells[1].GetStringValue());
-            Assert.AreEqual("Value:String:Address", cells[9].GetStringValue());
+            Assert.AreEqual("Value:String:Address", cells[2].GetStringValue());
+
+            document.Dispose();
+        }
+
+        [Test]
+        public void GetCellTest()
+        {
+            var document = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(fileName));
+            var worksheet = document.GetWorksheet(0);
+
+            var position = new ExcelCellIndex("B9");
+            var cell = worksheet.GetCell(position);
+            Assert.AreEqual("Model:Document:B10:C11", cell.GetStringValue());
+
+            document.Dispose();
+        }
+
+        [Test]
+        public void SearchCellsByTextTest()
+        {
+            var document = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(fileName));
+            var worksheet = document.GetWorksheet(0);
+
+            var cell = worksheet.SearchCellsByText("Value:String").FirstOrDefault();
+            Assert.AreEqual("Value:String:Name", cell.GetStringValue());
 
             document.Dispose();
         }
