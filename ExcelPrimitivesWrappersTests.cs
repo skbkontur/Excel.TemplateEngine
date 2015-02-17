bbde3e3adfb087a641e847e3e15f63d9a3ad9b1e
ageev_ps <ageev_ps@skbkontur.ru>
2015-02-17 16:20:22 +0500

    Core.ExcelFileGenerator: Реализация примитивов Template Engine для Excel

---
+++ ExcelPrimitivesWrappersTests.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
+
+using SKBKontur.Catalogue.ExcelFileGenerator;
+using SKBKontur.Catalogue.ExcelObjectPrinter.ExcelDocumentPrimitivesImplementation;
+using SKBKontur.Catalogue.ExcelObjectPrinter.NavigationPrimitives;
+
+namespace SKBKontur.Catalogue.Core.Tests.ExcelObjectPrinterTests
+{
+    [TestFixture]
+    public class ExcelPrimitivesWrappersTests
+    {
+        [Test]
+        public void ExcelTablePartExtractionTest()
+        {
+            var document = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(fileName));
+            var table = new ExcelTable(document.GetWorksheet(0));
+
+            var rows = table.GetTablePart(new CellPosition("B9"), new CellPosition("D11"))
+                            .Cells
+                            .Select(row => row.ToArray())
+                            .ToArray();
+
+            foreach(var cell in rows.SelectMany(row => row))
+                Assert.AreNotEqual(null, cell);
+
+            Assert.AreEqual("Template:RootTemplate:B10:D11", rows[0][0].StringValue);
+            Assert.AreEqual("", rows[0][1].StringValue + "");
+            Assert.AreEqual("", rows[0][2].StringValue + "");
+            Assert.AreEqual("Покупатель:", rows[1][0].StringValue);
+            Assert.AreEqual("Поставщик:", rows[1][1].StringValue);
+            Assert.AreEqual("", rows[1][2].StringValue + "");
+            Assert.AreEqual("Value:Organization:Buyer", rows[2][0].StringValue);
+            Assert.AreEqual("Value:Organization:Supplier", rows[2][1].StringValue);
+            Assert.AreEqual("Value::TypeName", rows[2][2].StringValue);
+        }
+
+        [Test]
+        public void ExcelCellExtractionTest()
+        {
+            var document = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(fileName));
+            var table = new ExcelTable(document.GetWorksheet(0));
+
+            var cell = table.GetCell(new CellPosition("B9"));
+            Assert.AreNotEqual(null, cell);
+            Assert.AreEqual("Template:RootTemplate:B10:D11", cell.StringValue);
+
+            cell = table.GetCell(new CellPosition("ABCD4234"));
+            Assert.AreEqual(null, cell);
+        }
+
+        private const string fileName = @"ExcelObjectPrinterTests\Files\template.xlsx";
+    }
+}
---
+++ ObjectExcelPrintingTests.cs
@@ -0,0 +1,165 @@
+﻿using System.IO;
+
+using NUnit.Framework;
+
+using SKBKontur.Catalogue.ExcelFileGenerator;
+using SKBKontur.Catalogue.ExcelObjectPrinter;
+using SKBKontur.Catalogue.ExcelObjectPrinter.ExcelDocumentPrimitivesImplementation;
+using SKBKontur.Catalogue.ExcelObjectPrinter.NavigationPrimitives;
+using SKBKontur.Catalogue.ExcelObjectPrinter.TableBuilder;
+
+namespace SKBKontur.Catalogue.Core.Tests.ExcelObjectPrinterTests
+{
+    [TestFixture]
+    public class ObjectExcelPrintingTests
+    {
+        [Test]
+        public void PrintStringTest()
+        {
+            const string model = "Test text";
+            MakeTest(model, simpleTemplateFileName);
+        }
+
+        [Test]
+        public void PrintSimpleObjectTest()
+        {
+            var model = new Document
+                {
+                    Buyer = new Organization
+                        {
+                            Address = "BuyerAddress",
+                            Name = "BuyerName"
+                        },
+                    Supplier = new Organization
+                        {
+                            Address = "SupplierAddress",
+                            Name = "SupplierName"
+                        },
+                    TypeName = "ORDERS"
+                };
+
+            MakeTest(model, simpleTemplateFileName);
+        }
+
+        [Test]
+        public void PrintObjectsByFullPathTest()
+        {
+            var model = new Document
+                {
+                    Buyer = new Organization
+                        {
+                            Address = "BuyerAddress",
+                            Name = "BuyerName"
+                        },
+                    Supplier = new Organization
+                        {
+                            Address = "SupplierAddress",
+                            Name = "SupplierName"
+                        },
+                    TypeName = "ORDERS"
+                };
+
+            MakeTest(model, withFullPathsTemplateFileName);
+        }
+
+        [Test]
+        public void PrintComplexObjectTest()
+        {
+            var model = new Document
+                {
+                    Buyer = new Organization
+                        {
+                            Address = "BuyerAddress",
+                            Name = "BuyerName"
+                        },
+                    Supplier = new Organization
+                        {
+                            Address = "SupplierAddress",
+                            Name = "SupplierName",
+                            Inn = 90238192,
+                            Kpp = "0832309812"
+                        },
+                    Payer = new Organization
+                        {
+                            Address = "PayerAddress",
+                            Name = "PayerAddress"
+                        },
+                    DeliveryParty = new Organization
+                        {
+                            Address = "DeliveryPartyAddress",
+                            Name = "DeliveryPartyName"
+                        },
+                    Vehicle = new VehicleInfo
+                        {
+                            NameOfCarrier = "Евкакий",
+                            TransportMode = "Agressive",
+                            VehicleBrand = "Tauria",
+                            VehicleNumber = "A777AB"
+                        },
+                    TypeName = "ORDERS"
+                };
+
+            MakeTest(model, complexTemplateFileName);
+        }
+
+        public class DocumentWithArray
+        {
+            public Organization[][] Array { get; set; }
+            public string NonArray { get; set; }
+        }
+
+        public class Document
+        {
+            public Organization DeliveryParty { get; set; }
+            public Organization Shipper { get; set; }
+            public Organization Invoicee { get; set; }
+            public Organization Supplier { get; set; }
+            public Organization Buyer { get; set; }
+            public Organization Payer { get; set; }
+            public string TypeName { get; set; }
+            public VehicleInfo Vehicle { get; set; }
+        }
+
+        public class Organization
+        {
+            public string Name { get; set; }
+            public int Inn { get; set; }
+            public string Kpp { get; set; }
+            public string Gln { get; set; }
+            public string Address { get; set; }
+            public string SupplierCodeInBuyerSystem { get; set; }
+        }
+
+        public class VehicleInfo
+        {
+            public string TransportMode { get; set; }
+            public string VehicleNumber { get; set; }
+            public string VehicleBrand { get; set; }
+            public string NameOfCarrier { get; set; }
+        }
+
+        private static void MakeTest(object model, string templateFileName)
+        {
+            var templateDocument = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(templateFileName));
+            var template = new ExcelTable(templateDocument.GetWorksheet(0));
+
+            var targetDocument = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(emptyDocumentName));
+            var target = new ExcelTable(targetDocument.GetWorksheet(0));
+
+            var tableBuilder = new TableBuilder(target, new CellPosition("B2"));
+            var templateEngine = new TemplateEngine(template);
+            templateEngine.Render(tableBuilder, model);
+
+            var result = targetDocument.GetDocumentBytes();
+            File.WriteAllBytes("output.xlsx", result);
+
+            templateDocument.Dispose();
+            targetDocument.Dispose();
+        }
+
+        private const string simpleTemplateFileName = @"ExcelObjectPrinterTests\Files\template.xlsx";
+        private const string complexTemplateFileName = @"ExcelObjectPrinterTests\Files\complexTemplate.xlsx";
+        private const string withFullPathsTemplateFileName = @"ExcelObjectPrinterTests\Files\withFullPathsTemplate.xlsx";
+        private const string emptyDocumentName = @"ExcelObjectPrinterTests\Files\empty.xlsx";
+    }
+}
