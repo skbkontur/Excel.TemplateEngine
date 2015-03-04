8355d83d279374824e45fa0bd965080c566546e4
ageev_ps <ageev_ps@skbkontur.ru>
2015-03-04 13:45:12 +0500

    Core.Excel: Добавление нового листа в документ

---
+++ WorksheetAdditionTests.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+using NUnit.Framework;
+
+using SKBKontur.Catalogue.ExcelFileGenerator;
+
+namespace SKBKontur.Catalogue.Core.Tests.ExcelFileGeneratorTests
+{
+    [TestFixture]
+    public class WorksheetAdditionTests
+    {
+        [Test]
+        public void WorksheetAdditionTest()
+        {
+            var document = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(emptyFileName));
+            document.AddWorksheet("Лист2");
+            var worksheet = document.GetWorksheet(1);
+            Assert.AreNotEqual(null, worksheet);
+
+            var result = document.GetDocumentBytes();
+            File.WriteAllBytes("output.xlsx", result);
+
+            document.Dispose();
+        }
+
+        private const string emptyFileName = @"ExcelFileGeneratorTests\Files\empty.xlsx";
+    }
+}
