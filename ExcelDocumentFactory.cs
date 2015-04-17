a0a658c20d7b99dd6ea705f6b18be2160032f7b1
ageev_ps <ageev_ps@skbkontur.ru>
2015-04-17 14:52:24 +0500

    Core.Excel: Инкапсулировал генерацию пустого документа в Core

--- ExcelDocumentFactory.cs
+++ ExcelDocumentFactory.cs
@@ -1,4 +1,8 @@
-﻿using SKBKontur.Catalogue.ExcelFileGenerator.Implementation.Primitives;
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using SKBKontur.Catalogue.ExcelFileGenerator.Implementation.Primitives;
 using SKBKontur.Catalogue.ExcelFileGenerator.Interfaces;
 
 namespace SKBKontur.Catalogue.ExcelFileGenerator
@@ -9,5 +13,26 @@ namespace SKBKontur.Catalogue.ExcelFileGenerator
         {
             return new ExcelDocument(template);
         }
+
+        public static IExcelDocument CreateEmpty()
+        {
+            return CreateFromTemplate(GetFileBytes(emptyFileName));
+        }
+
+        private static byte[] GetFileBytes(string filename)
+        {
+            var assembly = typeof(ExcelDocumentFactory).Assembly;
+            var manifestResourceName = assembly.GetManifestResourceNames().First(x => x.EndsWith(filename));
+            using(var stream = assembly.GetManifestResourceStream(manifestResourceName))
+            using(var ms = new MemoryStream())
+            {
+                if(stream == null)
+                    throw new Exception(string.Format("Resource '{0}' not found", filename));
+                stream.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        private const string emptyFileName = "empty.xlsx";
     }
 }
