b638989920d6d767f545f8d4dee539f21cd4a88c
andrew <Andrew.Kostousov@gmail.com>
2017-05-30 15:02:40 +0500

    code cleanup; using embedded resources to fix tests

--- ExcelDocumentFactory.cs
+++ ExcelDocumentFactory.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-
-using SKBKontur.Catalogue.ExcelFileGenerator.Implementation.Primitives;
+﻿using SKBKontur.Catalogue.ExcelFileGenerator.Implementation.Primitives;
 using SKBKontur.Catalogue.ExcelFileGenerator.Interfaces;
+using SKBKontur.Catalogue.Objects;
 
 namespace SKBKontur.Catalogue.ExcelFileGenerator
 {
@@ -16,23 +13,12 @@ namespace SKBKontur.Catalogue.ExcelFileGenerator
 
         public static IExcelDocument CreateEmpty()
         {
-            return CreateFromTemplate(GetFileBytes(emptyFileName));
+            return CreateFromTemplate(GetFileBytes("empty.xlsx"));
         }
 
         private static byte[] GetFileBytes(string filename)
         {
-            var assembly = typeof(ExcelDocumentFactory).Assembly;
-            var manifestResourceName = assembly.GetManifestResourceNames().First(x => x.EndsWith(filename));
-            using(var stream = assembly.GetManifestResourceStream(manifestResourceName))
-            using(var ms = new MemoryStream())
-            {
-                if(stream == null)
-                    throw new Exception(string.Format("Resource '{0}' not found", filename));
-                stream.CopyTo(ms);
-                return ms.ToArray();
-            }
+            return typeof(ExcelDocumentFactory).Assembly.ReadAllBytesFromResource(filename);
         }
-
-        private const string emptyFileName = "empty.xlsx";
     }
 }
