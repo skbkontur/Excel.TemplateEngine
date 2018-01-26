fe08f84a5ab2e9c1fb9532d60cdc71c74031ac37
Пивко Максим Александрович <mpivko@skbkontur.ru>
2018-01-26 11:43:41 +0500

    Add custom excel processing exceptions

---
+++ Exceptions/InvalidExcelDocumentException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using JetBrains.Annotations;
+
+namespace SKBKontur.Catalogue.ExcelFileGenerator.Exceptions
+{
+    public class InvalidExcelDocumentException : Exception
+    {
+        public InvalidExcelDocumentException()
+        {
+        }
+
+        public InvalidExcelDocumentException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidExcelDocumentException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected InvalidExcelDocumentException([NotNull] SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
