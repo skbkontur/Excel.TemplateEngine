e37da077e6d461d2d29f83817c494d368c8aebd4
mikheev.a <mikheev.a@skbkontur.ru>
2020-02-19 15:40:01 +0500

    add style tests

--- Excel.TemplateEngine.Tests/FileGeneratingTests/ExcelStyleTests.cs
+++ Excel.TemplateEngine.Tests/FileGeneratingTests/ExcelStyleTests.cs
@@ -6,6 +6,10 @@ using NUnit.Framework;
 
 using SkbKontur.Excel.TemplateEngine.FileGenerating;
 using SkbKontur.Excel.TemplateEngine.FileGenerating.DataTypes;
+using SkbKontur.Excel.TemplateEngine.ObjectPrinting.ExcelDocumentPrimitives.Implementations;
+using SkbKontur.Excel.TemplateEngine.ObjectPrinting.NavigationPrimitives.Implementations;
+using SkbKontur.Excel.TemplateEngine.ObjectPrinting.TableBuilder;
+using SkbKontur.Excel.TemplateEngine.ObjectPrinting.TableNavigator;
 
 using Vostok.Logging.Console;
 
@@ -42,13 +46,54 @@ namespace SkbKontur.Excel.TemplateEngine.Tests.FileGeneratingTests
         }
 
         [Test]
-        public void CellNumberFormatExtractionTest()
+        public void CellNumberFormatExtractionTest_Custom()
         {
             var templateDocument = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(GetFilePath("template.xlsx")), logger);
             var cell = templateDocument.GetWorksheet(0).GetCell(new ExcelCellIndex("A1"));
             var style = cell.GetStyle();
 
             style.NumberingFormat.Code.Should().Be("0.0000");
+        }
+
+        /// <summary>
+        ///     Test use one of standard numbering formats being stored without format code.
+        ///     To see all supported standard formats look for ExcelDocumentStyle.standardNumberingFormatsId
+        /// </summary>
+        [Test]
+        public void CellNumberFormatExtractionTest_Standard()
+        {
+            var templateDocument = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(GetFilePath("template.xlsx")), logger);
+            var cell = templateDocument.GetWorksheet(0).GetCell(new ExcelCellIndex("A4"));
+            var style = cell.GetStyle();
+
+            style.NumberingFormat.Id.Should().Be(49);
+            style.NumberingFormat.Code.Should().BeNull();
+        }
+
+        [Test]
+        public void CellNumberFormatCopyAfterRenderTest()
+        {
+            using (var templateDocument = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(GetFilePath("template.xlsx")), logger))
+            {
+                var template = new ExcelTable(templateDocument.GetWorksheet(0));
+                var templateEngine = new TemplateEngine(template, logger);
+
+                using (var targetDocument = ExcelDocumentFactory.CreateFromTemplate(File.ReadAllBytes(GetFilePath("empty.xlsx")), logger))
+                {
+                    var target = new ExcelTable(targetDocument.GetWorksheet(0));
+                    var tableBuilder = new TableBuilder(target, new TableNavigator(new CellPosition("A1"), logger), new Style(template.GetCell(new CellPosition("A1"))));
+
+                    templateEngine.Render(tableBuilder, new {A = 1, B = "asdf"});
+
+                    var worksheet = targetDocument.GetWorksheet(0);
+                    var customStyle = worksheet.GetCell(new ExcelCellIndex("A1")).GetStyle();
+                    customStyle.NumberingFormat.Code.Should().Be("0.0000");
+
+                    var standardStyle = worksheet.GetCell(new ExcelCellIndex("A4")).GetStyle();
+                    standardStyle.NumberingFormat.Id.Should().Be(49);
+                    standardStyle.NumberingFormat.Code.Should().BeNull();
+                }
+            }
         }
 
         [Test]
