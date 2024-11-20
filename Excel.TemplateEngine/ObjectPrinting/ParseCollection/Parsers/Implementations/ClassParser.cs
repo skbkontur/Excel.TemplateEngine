e8a09344d4da3629acde15b38a3b4ba54ad14e79
Ярин Захар Владимирович <z.yarin@skbkontur.ru>
2024-11-20 16:28:11 +0500

    parse multiple worksheets into one model object

--- Excel.TemplateEngine/ObjectPrinting/ParseCollection/Parsers/Implementations/ClassParser.cs
+++ Excel.TemplateEngine/ObjectPrinting/ParseCollection/Parsers/Implementations/ClassParser.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-using JetBrains.Annotations;
-
 using SkbKontur.Excel.TemplateEngine.ObjectPrinting.ExcelDocumentPrimitives;
 using SkbKontur.Excel.TemplateEngine.ObjectPrinting.Helpers;
 using SkbKontur.Excel.TemplateEngine.ObjectPrinting.RenderingTemplates;
@@ -12,161 +10,180 @@ using SkbKontur.Excel.TemplateEngine.ObjectPrinting.TableParser;
 
 using Vostok.Logging.Abstractions;
 
-namespace SkbKontur.Excel.TemplateEngine.ObjectPrinting.ParseCollection.Parsers.Implementations
+#nullable enable
+namespace SkbKontur.Excel.TemplateEngine.ObjectPrinting.ParseCollection.Parsers.Implementations;
+
+internal class ClassParser : IClassParser
 {
-    internal class ClassParser : IClassParser
+    public ClassParser(IParserCollection parserCollection, ILog logger)
     {
-        public ClassParser(IParserCollection parserCollection, ILog logger)
-        {
-            this.parserCollection = parserCollection;
-            this.logger = logger;
-        }
-
-        [NotNull]
-        public TModel Parse<TModel>([NotNull] ITableParser tableParser, [NotNull] RenderingTemplate template, [NotNull] Action<string, string> addFieldMapping)
-            where TModel : new()
-        {
-            var model = new TModel();
-
-            var enumerablesLengths = GetEnumerablesLengths<TModel>(tableParser, template);
-
-            foreach (var row in template.Content.Cells)
-            {
-                foreach (var cell in row)
-                {
-                    tableParser.PushState(cell.CellPosition);
-
-                    var expression = cell.StringValue;
-
-                    if (TemplateDescriptionHelper.IsCorrectValueDescription(expression))
-                    {
-                        ParseCellularValue(tableParser, addFieldMapping, model, ExcelTemplatePath.FromRawExpression(expression), enumerablesLengths);
-                        continue;
-                    }
-                    if (TemplateDescriptionHelper.IsCorrectFormValueDescription(expression))
-                    {
-                        ParseFormValue(tableParser, addFieldMapping, model, cell, ExcelTemplatePath.FromRawExpression(expression));
-                        continue;
-                    }
-
-                    tableParser.PopState();
-                }
-            }
-
-            return model;
-        }
-
-        [NotNull]
-        private Dictionary<ExcelTemplatePath, int> GetEnumerablesLengths<TModel>([NotNull] ITableParser tableParser, [NotNull] RenderingTemplate template)
-        {
-            var enumerableCellsGroups = new Dictionary<ExcelTemplatePath, List<ICell>>();
-            foreach (var row in template.Content.Cells)
-            {
-                foreach (var cell in row)
-                {
-                    var expression = cell.StringValue;
-
-                    if (TemplateDescriptionHelper.IsCorrectValueDescription(expression) && ExcelTemplatePath.FromRawExpression(expression).HasArrayAccess)
-                    {
-                        var cleanPathToEnumerable = ExcelTemplatePath.FromRawExpression(expression)
-                                                                     .SplitForEnumerableExpansion()
-                                                                     .pathToEnumerable
-                                                                     .WithoutArrayAccess();
-                        if (!enumerableCellsGroups.ContainsKey(cleanPathToEnumerable))
-                            enumerableCellsGroups[cleanPathToEnumerable] = new List<ICell>();
-                        enumerableCellsGroups[cleanPathToEnumerable].Add(cell);
-                    }
-                }
-            }
-
-            var enumerablesLengths = new Dictionary<ExcelTemplatePath, int>();
-
-            foreach (var enumerableCells in enumerableCellsGroups)
-            {
-                var cleanPathToEnumerable = enumerableCells.Key;
-
-                var childEnumerableType = ObjectPropertiesExtractor.ExtractChildObjectTypeFromPath(typeof(TModel), cleanPathToEnumerable);
-                if (!TypeCheckingHelper.IsIList(childEnumerableType))
-                    throw new InvalidOperationException($"Only ILists are supported as collections, but tried to use '{childEnumerableType}'. (path: {cleanPathToEnumerable.RawPath})");
-
-                var primaryParts = enumerableCells.Value.Where(x => ExcelTemplatePath.FromRawExpression(x.StringValue).HasPrimaryKeyArrayAccess).ToList();
-                if (primaryParts.Count == 0)
-                    primaryParts = enumerableCells.Value.Take(1).ToList();
-
-                var measurer = parserCollection.GetEnumerableMeasurer();
-                enumerablesLengths[cleanPathToEnumerable] = measurer.GetLength(tableParser, typeof(TModel), primaryParts);
-            }
-
-            return enumerablesLengths;
-        }
-
-        private void ParseCellularValue(ITableParser tableParser, Action<string, string> addFieldMapping, object model, ExcelTemplatePath path, Dictionary<ExcelTemplatePath, int> enumerablesLengths)
-        {
-            var modelType = model.GetType();
-            var leafSetter = ObjectChildSetterFactory.GetChildObjectSetter(modelType, path);
-            var leafModelType = ObjectPropertiesExtractor.ExtractChildObjectTypeFromPath(modelType, path);
-
-            if (path.HasArrayAccess)
-                ParseEnumerableValue(tableParser, addFieldMapping, model, path, leafSetter, leafModelType, enumerablesLengths);
-            else
-                ParseSingleValue(tableParser, addFieldMapping, model, leafSetter, path, leafModelType);
-        }
-
-        private void ParseSingleValue(ITableParser tableParser, Action<string, string> addFieldMapping, object model, Action<object, object> leafSetter, ExcelTemplatePath childModelPath, Type childModelType)
-        {
-            addFieldMapping(childModelPath.RawPath, tableParser.CurrentState.Cursor.CellReference);
-            if (!TextValueParser.TryParse(tableParser.GetCurrentCellText(), childModelType, out var parsedObject))
-            {
-                logger.Error($"Failed to parse value from '{tableParser.CurrentState.Cursor.CellReference}' with childModelType='{childModelType}' via AtomicValueParser");
-                return;
-            }
-            leafSetter(model, parsedObject);
-        }
-
-        private void ParseEnumerableValue(ITableParser tableParser, Action<string, string> addFieldMapping, object model, ExcelTemplatePath path, Action<object, object> leafSetter, Type leafModelType, Dictionary<ExcelTemplatePath, int> enumerablesLengths)
-        {
-            var (rawPathToEnumerable, childPath) = path.SplitForEnumerableExpansion();
-
-            var cleanPathToEnumerable = rawPathToEnumerable.WithoutArrayAccess();
-
-            var enumerableType = ObjectPropertiesExtractor.ExtractChildObjectTypeFromPath(model.GetType(), cleanPathToEnumerable);
-            if (!typeof(IList).IsAssignableFrom(enumerableType))
-                throw new Exception($"Only ILists are supported as collections, but tried to use '{enumerableType}'. (path: {cleanPathToEnumerable.RawPath})");
-
-            var parser = parserCollection.GetEnumerableParser(enumerableType);
-
-            var count = enumerablesLengths[cleanPathToEnumerable];
-            var parsedList = parser.Parse(tableParser, leafModelType, count, (name, value) => addFieldMapping($"{cleanPathToEnumerable.RawPath}{name}.{childPath.RawPath}", value));
-
-            leafSetter(model, parsedList);
-        }
-
-        private void ParseFormValue(ITableParser tableParser, Action<string, string> addFieldMapping, object model, ICell cell, ExcelTemplatePath path)
-        {
-            var modelType = model.GetType();
-            var childSetter = ObjectChildSetterFactory.GetChildObjectSetter(modelType, path);
-            var childModelType = ObjectPropertiesExtractor.ExtractChildObjectTypeFromPath(modelType, path);
-            var (childFormControlType, childFormControlName) = GetFormControlDescription(cell);
-
-            if (path.HasArrayAccess)
-                throw new InvalidOperationException("Enumerables are not supported for form controls");
-
-            var parser = parserCollection.GetFormValueParser(childFormControlType, childModelType);
-            var parsedObject = parser.ParseOrDefault(tableParser, childFormControlName, childModelType);
-
-            childSetter(model, parsedObject);
-            addFieldMapping(path.RawPath, childFormControlName);
-        }
-
-        private static (string formControlType, string formControlName) GetFormControlDescription([NotNull] ICell cell)
-        {
-            var formControlDescription = TemplateDescriptionHelper.TryGetFormControlFromValueDescription(cell.StringValue);
-            if (string.IsNullOrEmpty(formControlDescription.formControlType) || formControlDescription.formControlName == null)
-                throw new InvalidOperationException($"Invalid xlsx template. '{cell.StringValue}' is not a valid form control description.");
-            return formControlDescription;
-        }
-
-        private readonly IParserCollection parserCollection;
-        private readonly ILog logger;
+        this.parserCollection = parserCollection;
+        this.logger = logger;
     }
+
+    public TModel Parse<TModel>(ITableParser tableParser, RenderingTemplate template, Action<string, string> addFieldMapping)
+        where TModel : new()
+    {
+        var model = new TModel();
+        Parse(tableParser, template, addFieldMapping, ref model);
+        return model;
+    }
+
+    public void Parse<TModel>(ITableParser tableParser, RenderingTemplate template, Action<string, string> addFieldMapping, ref TModel model)
+        where TModel : new()
+    {
+        if (model == null) throw new ArgumentNullException(nameof(model));
+
+        var enumerablesLengths = GetEnumerablesLengths<TModel>(tableParser, template);
+
+        foreach (var row in template.Content.Cells)
+        {
+            foreach (var cell in row)
+            {
+                tableParser.PushState(cell.CellPosition);
+
+                var expression = cell.StringValue;
+
+                if (TemplateDescriptionHelper.IsCorrectValueDescription(expression))
+                {
+                    ParseCellularValue(tableParser, addFieldMapping, model, ExcelTemplatePath.FromRawExpression(expression), enumerablesLengths);
+                    continue;
+                }
+                if (TemplateDescriptionHelper.IsCorrectFormValueDescription(expression))
+                {
+                    ParseFormValue(tableParser, addFieldMapping, model, cell, ExcelTemplatePath.FromRawExpression(expression));
+                    continue;
+                }
+
+                tableParser.PopState();
+            }
+        }
+    }
+
+    private Dictionary<ExcelTemplatePath, int> GetEnumerablesLengths<TModel>(ITableParser tableParser, RenderingTemplate template)
+    {
+        var enumerableCellsGroups = new Dictionary<ExcelTemplatePath, List<ICell>>();
+        foreach (var row in template.Content.Cells)
+        {
+            foreach (var cell in row)
+            {
+                var expression = cell.StringValue;
+
+                if (TemplateDescriptionHelper.IsCorrectValueDescription(expression) && ExcelTemplatePath.FromRawExpression(expression).HasArrayAccess)
+                {
+                    var cleanPathToEnumerable = ExcelTemplatePath.FromRawExpression(expression)
+                                                                 .SplitForEnumerableExpansion()
+                                                                 .pathToEnumerable
+                                                                 .WithoutArrayAccess();
+                    if (!enumerableCellsGroups.ContainsKey(cleanPathToEnumerable))
+                        enumerableCellsGroups[cleanPathToEnumerable] = new List<ICell>();
+                    enumerableCellsGroups[cleanPathToEnumerable].Add(cell);
+                }
+            }
+        }
+
+        var enumerablesLengths = new Dictionary<ExcelTemplatePath, int>();
+
+        foreach (var enumerableCells in enumerableCellsGroups)
+        {
+            var cleanPathToEnumerable = enumerableCells.Key;
+
+            var childEnumerableType = ObjectPropertiesExtractor.ExtractChildObjectTypeFromPath(typeof(TModel), cleanPathToEnumerable);
+            if (!TypeCheckingHelper.IsIList(childEnumerableType))
+                throw new InvalidOperationException($"Only ILists are supported as collections, but tried to use '{childEnumerableType}'. (path: {cleanPathToEnumerable.RawPath})");
+
+            var primaryParts = enumerableCells.Value.Where(x => ExcelTemplatePath.FromRawExpression(x.StringValue).HasPrimaryKeyArrayAccess).ToList();
+            if (primaryParts.Count == 0)
+                primaryParts = enumerableCells.Value.Take(1).ToList();
+
+            var measurer = parserCollection.GetEnumerableMeasurer();
+            enumerablesLengths[cleanPathToEnumerable] = measurer.GetLength(tableParser, typeof(TModel), primaryParts);
+        }
+
+        return enumerablesLengths;
+    }
+
+    private void ParseCellularValue(ITableParser tableParser,
+                                    Action<string, string> addFieldMapping,
+                                    object model,
+                                    ExcelTemplatePath path,
+                                    Dictionary<ExcelTemplatePath, int> enumerablesLengths)
+    {
+        var modelType = model.GetType();
+        var leafSetter = ObjectChildSetterFactory.GetChildObjectSetter(modelType, path);
+        var leafModelType = ObjectPropertiesExtractor.ExtractChildObjectTypeFromPath(modelType, path);
+
+        if (path.HasArrayAccess)
+            ParseEnumerableValue(tableParser, addFieldMapping, model, path, leafSetter, leafModelType, enumerablesLengths);
+        else
+            ParseSingleValue(tableParser, addFieldMapping, model, leafSetter, path, leafModelType);
+    }
+
+    private void ParseSingleValue(ITableParser tableParser,
+                                  Action<string, string> addFieldMapping,
+                                  object model,
+                                  Action<object, object> leafSetter,
+                                  ExcelTemplatePath childModelPath,
+                                  Type childModelType)
+    {
+        addFieldMapping(childModelPath.RawPath, tableParser.CurrentState.Cursor.CellReference);
+        if (!TextValueParser.TryParse(tableParser.GetCurrentCellText(), childModelType, out var parsedObject))
+        {
+            logger.Error($"Failed to parse value from '{tableParser.CurrentState.Cursor.CellReference}' with childModelType='{childModelType}' via AtomicValueParser");
+            return;
+        }
+        leafSetter(model, parsedObject);
+    }
+
+    private void ParseEnumerableValue(ITableParser tableParser,
+                                      Action<string, string> addFieldMapping,
+                                      object model,
+                                      ExcelTemplatePath path,
+                                      Action<object, object> leafSetter,
+                                      Type leafModelType,
+                                      Dictionary<ExcelTemplatePath, int> enumerablesLengths)
+    {
+        var (rawPathToEnumerable, childPath) = path.SplitForEnumerableExpansion();
+
+        var cleanPathToEnumerable = rawPathToEnumerable.WithoutArrayAccess();
+
+        var enumerableType = ObjectPropertiesExtractor.ExtractChildObjectTypeFromPath(model.GetType(), cleanPathToEnumerable);
+        if (!typeof(IList).IsAssignableFrom(enumerableType))
+            throw new Exception($"Only ILists are supported as collections, but tried to use '{enumerableType}'. (path: {cleanPathToEnumerable.RawPath})");
+
+        var parser = parserCollection.GetEnumerableParser(enumerableType);
+
+        var count = enumerablesLengths[cleanPathToEnumerable];
+        var parsedList = parser.Parse(tableParser, leafModelType, count, (name, value) => addFieldMapping($"{cleanPathToEnumerable.RawPath}{name}.{childPath.RawPath}", value));
+
+        leafSetter(model, parsedList);
+    }
+
+    private void ParseFormValue(ITableParser tableParser, Action<string, string> addFieldMapping, object model, ICell cell, ExcelTemplatePath path)
+    {
+        var modelType = model.GetType();
+        var childSetter = ObjectChildSetterFactory.GetChildObjectSetter(modelType, path);
+        var childModelType = ObjectPropertiesExtractor.ExtractChildObjectTypeFromPath(modelType, path);
+        var (childFormControlType, childFormControlName) = GetFormControlDescription(cell);
+
+        if (path.HasArrayAccess)
+            throw new InvalidOperationException("Enumerables are not supported for form controls");
+
+        var parser = parserCollection.GetFormValueParser(childFormControlType, childModelType);
+        var parsedObject = parser.ParseOrDefault(tableParser, childFormControlName, childModelType);
+
+        childSetter(model, parsedObject);
+        addFieldMapping(path.RawPath, childFormControlName);
+    }
+
+    private static (string formControlType, string formControlName) GetFormControlDescription(ICell cell)
+    {
+        var formControlDescription = TemplateDescriptionHelper.TryGetFormControlFromValueDescription(cell.StringValue);
+        if (string.IsNullOrEmpty(formControlDescription.formControlType) || formControlDescription.formControlName == null)
+            throw new InvalidOperationException($"Invalid xlsx template. '{cell.StringValue}' is not a valid form control description.");
+        return formControlDescription;
+    }
+
+    private readonly IParserCollection parserCollection;
+    private readonly ILog logger;
 }
--- Excel.TemplateEngine/TemplateEngine.cs
+++ Excel.TemplateEngine/TemplateEngine.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 
-using JetBrains.Annotations;
-
 using SkbKontur.Excel.TemplateEngine.ObjectPrinting.ExcelDocumentPrimitives;
 using SkbKontur.Excel.TemplateEngine.ObjectPrinting.LazyParse;
 using SkbKontur.Excel.TemplateEngine.ObjectPrinting.NavigationPrimitives.Implementations;
@@ -14,70 +12,75 @@ using SkbKontur.Excel.TemplateEngine.ObjectPrinting.TableParser;
 
 using Vostok.Logging.Abstractions;
 
-namespace SkbKontur.Excel.TemplateEngine
+#nullable enable
+namespace SkbKontur.Excel.TemplateEngine;
+
+public class TemplateEngine : ITemplateEngine
 {
-    public class TemplateEngine : ITemplateEngine
+    public TemplateEngine(ITable templateTable, ILog logger)
     {
-        public TemplateEngine([NotNull] ITable templateTable, [NotNull] ILog logger)
-        {
-            this.templateTable = templateTable;
-            templateCollection = new TemplateCollection(templateTable);
-            rendererCollection = new RendererCollection(templateCollection);
-            parserCollection = new ParserCollection(logger.ForContext("ExcelObjectPrinter"));
-        }
-
-        public void Render<TModel>([NotNull] ITableBuilder tableBuilder, [NotNull] TModel model)
-        {
-            var renderingTemplate = templateCollection.GetTemplate(rootTemplateName)
-                                    ?? throw new InvalidOperationException($"Template with name {rootTemplateName} not found in xlsx");
-            tableBuilder.CopyFormControlsFrom(templateTable);
-            tableBuilder.CopyDataValidationsFrom(templateTable);
-            tableBuilder.CopyWorksheetExtensionListFrom(templateTable); // WorksheetExtensionList contains info about data validations with ranges from other sheets, so copying it to support them.
-            tableBuilder.CopyCommentsFrom(templateTable);
-            var render = rendererCollection.GetRenderer(model.GetType());
-            render.Render(tableBuilder, model, renderingTemplate);
-        }
-
-        public (TModel model, Dictionary<string, string> mappingForErrors) Parse<TModel>([NotNull] ITableParser tableParser)
-            where TModel : new()
-        {
-            var renderingTemplate = templateCollection.GetTemplate(rootTemplateName)
-                                    ?? throw new InvalidOperationException($"Template with name {rootTemplateName} not found in xlsx");
-            var parser = parserCollection.GetClassParser();
-            var fieldsMappingForErrors = new Dictionary<string, string>();
-            return (model : parser.Parse<TModel>(tableParser, renderingTemplate, (name, value) => fieldsMappingForErrors.Add(name, value)), mappingForErrors : fieldsMappingForErrors);
-        }
-
-        /// <summary>
-        ///     Parse only separate cell values and List<> enumerations without and size limitations.
-        /// </summary>
-        /// <typeparam name="TModel">Class to parse.</typeparam>
-        /// <param name="lazyTableReader">LazyTableReader of target xlsx file.</param>
-        /// <param name="readerOffset">Target file offset relative to a template.</param>
-        /// <param name="formulaEvaluator">Target file formula evaluator.</param>
-        public TModel LazyParse<TModel>([NotNull] LazyTableReader lazyTableReader, ObjectSize readerOffset = null, IFormulaEvaluator formulaEvaluator = null)
-            where TModel : new()
-        {
-            readerOffset ??= new ObjectSize(0, 0);
-
-            var renderingTemplate = templateCollection.GetTemplate(rootTemplateName) ??
-                                    throw new InvalidOperationException($"Template with name {rootTemplateName} not found in xlsx");
-            var parser = parserCollection.GetLazyClassParser();
-            return parser.Parse<TModel>(lazyTableReader, renderingTemplate, readerOffset, formulaEvaluator);
-        }
-
-        private const string rootTemplateName = "RootTemplate";
-
-        [NotNull]
-        private readonly ITable templateTable;
-
-        [NotNull]
-        private readonly ITemplateCollection templateCollection;
-
-        [NotNull]
-        private readonly IRendererCollection rendererCollection;
-
-        [NotNull]
-        private readonly IParserCollection parserCollection;
+        this.templateTable = templateTable;
+        templateCollection = new TemplateCollection(templateTable);
+        rendererCollection = new RendererCollection(templateCollection);
+        parserCollection = new ParserCollection(logger.ForContext("ExcelObjectPrinter"));
     }
+
+    public void Render<TModel>(ITableBuilder tableBuilder, TModel model) where TModel : notnull
+    {
+        var renderingTemplate = templateCollection.GetTemplate(rootTemplateName)
+                                ?? throw new InvalidOperationException($"Template with name {rootTemplateName} not found in xlsx");
+        tableBuilder.CopyFormControlsFrom(templateTable);
+        tableBuilder.CopyDataValidationsFrom(templateTable);
+        tableBuilder.CopyWorksheetExtensionListFrom(templateTable); // WorksheetExtensionList contains info about data validations with ranges from other sheets, so copying it to support them.
+        tableBuilder.CopyCommentsFrom(templateTable);
+        var render = rendererCollection.GetRenderer(model.GetType());
+        render.Render(tableBuilder, model, renderingTemplate);
+    }
+
+    public (TModel model, Dictionary<string, string> mappingForErrors) Parse<TModel>(ITableParser tableParser)
+        where TModel : new()
+    {
+        var renderingTemplate = templateCollection.GetTemplate(rootTemplateName)
+                                ?? throw new InvalidOperationException($"Template with name {rootTemplateName} not found in xlsx");
+        var parser = parserCollection.GetClassParser();
+        var fieldsMappingForErrors = new Dictionary<string, string>();
+        return (model : parser.Parse<TModel>(tableParser, renderingTemplate, (name, value) => fieldsMappingForErrors.Add(name, value)), mappingForErrors : fieldsMappingForErrors);
+    }
+
+    public void Parse<TModel>(ITableParser tableParser, Action<string, string> mappingForErrors, ref TModel model)
+        where TModel : new()
+    {
+        var renderingTemplate = templateCollection.GetTemplate(rootTemplateName)
+                                ?? throw new InvalidOperationException($"Template with name {rootTemplateName} not found in xlsx");
+        var parser = parserCollection.GetClassParser();
+        parser.Parse(tableParser, renderingTemplate, mappingForErrors, ref model);
+    }
+
+    /// <summary>
+    ///     Parse only separate cell values and List<> enumerations without and size limitations.
+    /// </summary>
+    /// <typeparam name="TModel">Class to parse.</typeparam>
+    /// <param name="lazyTableReader">LazyTableReader of target xlsx file.</param>
+    /// <param name="readerOffset">Target file offset relative to a template.</param>
+    /// <param name="formulaEvaluator">Target file formula evaluator.</param>
+    public TModel LazyParse<TModel>(LazyTableReader lazyTableReader, ObjectSize? readerOffset = null, IFormulaEvaluator? formulaEvaluator = null)
+        where TModel : new()
+    {
+        readerOffset ??= new ObjectSize(0, 0);
+
+        var renderingTemplate = templateCollection.GetTemplate(rootTemplateName) ??
+                                throw new InvalidOperationException($"Template with name {rootTemplateName} not found in xlsx");
+        var parser = parserCollection.GetLazyClassParser();
+        return parser.Parse<TModel>(lazyTableReader, renderingTemplate, readerOffset, formulaEvaluator);
+    }
+
+    private const string rootTemplateName = "RootTemplate";
+
+    private readonly ITable templateTable;
+
+    private readonly ITemplateCollection templateCollection;
+
+    private readonly IRendererCollection rendererCollection;
+
+    private readonly IParserCollection parserCollection;
 }
